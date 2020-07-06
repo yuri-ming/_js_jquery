fde91f9ae97278771070d289453d3710015da97f
yuri-ming <lul6959@naver.com>
2020-07-06 16:10:44 +0900

    test

---
+++ basic1.js
@@ -0,0 +1,48 @@
+//number
+var num1=1001;
+var num2=10.50;
+
+//string
+var string1='Hello';
+var string2="world";
+
+//Boolean
+var bool=true;
+
+//null
+var foo=null;
+
+//undefined
+var bar;
+
+//Objcet
+var obj={name:'Lee', gender:'male'};
+
+//Array
+var array=[1,2,3];
+
+//function
+var foo = function() {};
+
+//산술연산자
+var area= 5*4;
+
+//문자열 연결 연산자
+var str='My name is' + 'Lee';
+ 
+//할당 연산자
+var color='red';
+
+//비교 연산자
+var foo=3>5;
+
+//논리 연산자
+var bar=(5>3) && (2<4);
+
+//타입 연산자
+var type = typeof 'Hi';
+
+var today = new Date();
+
+var time= 10; //변수선언 var
+var greeting;
