31777ffa31d259fdb1de646bab722f393313b933
yuri-ming <lul6959@naver.com>
2020-07-06 16:12:12 +0900

    블록문 개념

---
+++ js_1/block.js
@@ -0,0 +1,18 @@
+//블록문
+{var foo =10;
+console.log(foo);
+}
+
+//제어문
+var x= 0;
+while(x<10){
+    x++;
+}
+console.log(x);
+
+//함수 선언문
+function sum(x,y){
+    return x+y;
+
+}
+console.log(sum(1,2));
